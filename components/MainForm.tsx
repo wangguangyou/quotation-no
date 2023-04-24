53a5dab01032031407f9f172d31b96394c5c10c9
wangguangyou <15968848474@100fen.cn>
2023-04-24 16:21:25 +0800

    报价单

--- components/MainForm.tsx
+++ components/MainForm.tsx
@@ -1,4 +1,4 @@
-import { Form, Input, Button } from 'antd'
+import { Form, Input, Button, InputNumber, Select, Switch } from 'antd'
 
 const MainForm = () => {
   const onFinish = async (values: any) => {
@@ -8,10 +8,10 @@ const MainForm = () => {
   return (
     <div className="relative">
       <Form onFinish={onFinish}>
-        <div className="pb-150 space-y-32">
+        <div className="pb-130 space-y-32">
           <div>
-            <div className="divider mb-24">
-              <div className="text-#888 inline-block text-20 fw-600 mb-12">
+            <div className="divider mb-32">
+              <div className="text-#666 inline-block text-20 fw-600 mb-12">
                 客户资料
               </div>
             </div>
@@ -45,54 +45,321 @@ const MainForm = () => {
             </Form.Item>
           </div>
           <div>
-            <div className="divider mb-24">
-              <div className="text-#888  inline-block text-20 fw-600 mb-12">
+            <div className="divider mb-32">
+              <div className="text-#666  inline-block text-20 fw-600 mb-12">
                 产品基础资料
               </div>
             </div>
+            <div className="pl-24">
+              <div className="text-#666 text-16 mb-12">产品尺寸</div>
+
+              <div className="fi space-x-24">
+                <Form.Item
+                  labelCol={{ span: 7 }}
+                  label="长(CM)"
+                  name="l"
+                  rules={[{ required: true, message: '此项必填' }]}
+                >
+                  <InputNumber
+                    min={0}
+                    className="w-200"
+                    placeholder="请输入内容"
+                  />
+                </Form.Item>
+                <Form.Item
+                  labelCol={{ span: 7 }}
+                  label="宽(CM)"
+                  name="w"
+                  rules={[{ required: true, message: '此项必填' }]}
+                >
+                  <InputNumber
+                    min={0}
+                    className="w-200"
+                    placeholder="请输入内容"
+                  />
+                </Form.Item>
+                <Form.Item
+                  labelCol={{ span: 7 }}
+                  label="厚(MM)"
+                  name="h"
+                  rules={[{ required: true, message: '此项必填' }]}
+                >
+                  <InputNumber
+                    min={0}
+                    className="w-200"
+                    placeholder="请输入内容"
+                  />
+                </Form.Item>
+              </div>
+            </div>
+            <div className="pl-24 ">
+              <div className="text-#666 text-16 mb-12">产品数量</div>
+
+              <div className="fi space-x-24">
+                <Form.Item
+                  label="订单数量"
+                  name="sum"
+                  rules={[{ required: true, message: '此项必填' }]}
+                >
+                  <InputNumber
+                    min={0}
+                    className="w-200!"
+                    placeholder="请输入内容"
+                    addonAfter="PCS"
+                  />
+                </Form.Item>
+                <Form.Item
+                  label="印刷方式"
+                  name="ysfs"
+                  rules={[{ required: true, message: '此项必填' }]}
+                >
+                  <Select
+                    placeholder="请选择"
+                    className="w-200!"
+                    options={[{ value: 'lucy', label: 'Lucy' }]}
+                  />
+                </Form.Item>
+                <Form.Item
+                  label="边缘处理方式"
+                  name="byclfs"
+                  rules={[{ required: true, message: '此项必填' }]}
+                >
+                  <Select
+                    placeholder="请选择"
+                    className="w-200!"
+                    options={[{ value: 'lucy', label: 'Lucy' }]}
+                  />
+                </Form.Item>
+              </div>
+            </div>
           </div>
 
           <div>
-            <div className="divider mb-24">
-              <div className="text-#888 inline-block text-20 fw-600 mb-12">
+            <div className="divider mb-32">
+              <div className="text-#666 inline-block text-20 fw-600 mb-12">
                 主材料&辅料
               </div>
             </div>
+
+            <div className="pl-24">
+              <div className="text-#666 text-16 mb-12">材质</div>
+
+              <div className="fi space-x-24">
+                <Form.Item
+                  label="面料"
+                  name="ysfs"
+                  rules={[{ required: true, message: '此项必填' }]}
+                >
+                  <Select
+                    placeholder="请选择"
+                    className="w-200!"
+                    options={[{ value: 'lucy', label: 'Lucy' }]}
+                  />
+                </Form.Item>
+                <Form.Item
+                  label="底料"
+                  name="byclfs"
+                  rules={[{ required: true, message: '此项必填' }]}
+                >
+                  <Select
+                    placeholder="请选择"
+                    className="w-200!"
+                    options={[{ value: 'lucy', label: 'Lucy' }]}
+                  />
+                </Form.Item>
+              </div>
+
+              <div className="text-#666 text-16 mb-12">辅料明细</div>
+            </div>
           </div>
 
           <div>
-            <div className="divider mb-24">
-              <div className="text-#888 inline-block text-20 fw-600 mb-12">
+            <div className="divider mb-32">
+              <div className="text-#666 inline-block text-20 fw-600 mb-12">
                 装箱资料
               </div>
             </div>
-          </div>
 
-          <div>
-            <div className="divider mb-24">
-              <div className="text-#888 inline-block text-20 fw-600 mb-12">
-                物流运输
+            <div className="pl-24">
+              <div className="text-#666 text-16 mb-12">包装要求</div>
+
+              <div className="fi space-x-24">
+                <Form.Item
+                  label="摆放方式"
+                  name="ysfs"
+                  rules={[{ required: true, message: '此项必填' }]}
+                >
+                  <Select
+                    placeholder="请选择"
+                    className="w-200!"
+                    options={[{ value: 'lucy', label: 'Lucy' }]}
+                  />
+                </Form.Item>
+              </div>
+
+              <div className="text-#666 text-16 mb-12">纸箱尺寸</div>
+              <div className="fi space-x-24">
+                <Form.Item
+                  labelCol={{ span: 7 }}
+                  label="长(CM)"
+                  name="l"
+                  rules={[{ required: true, message: '此项必填' }]}
+                >
+                  <InputNumber
+                    min={0}
+                    className="w-200"
+                    placeholder="请输入内容"
+                  />
+                </Form.Item>
+                <Form.Item
+                  labelCol={{ span: 7 }}
+                  label="宽(CM)"
+                  name="w"
+                  rules={[{ required: true, message: '此项必填' }]}
+                >
+                  <InputNumber
+                    min={0}
+                    className="w-200"
+                    placeholder="请输入内容"
+                  />
+                </Form.Item>
+                <Form.Item
+                  labelCol={{ span: 7 }}
+                  label="厚(MM)"
+                  name="h"
+                  rules={[{ required: true, message: '此项必填' }]}
+                >
+                  <InputNumber
+                    min={0}
+                    className="w-200"
+                    placeholder="请输入内容"
+                  />
+                </Form.Item>
+              </div>
+              <div className="fi space-x-24">
+                <Form.Item
+                  labelCol={{ span: 7 }}
+                  label="整箱数量"
+                  name="l"
+                  rules={[{ required: true, message: '此项必填' }]}
+                >
+                  <InputNumber
+                    min={0}
+                    className="w-200"
+                    placeholder="请输入内容"
+                  />
+                </Form.Item>
+                <Form.Item
+                  labelCol={{ span: 7 }}
+                  label="整箱毛重"
+                  name="w"
+                  rules={[{ required: true, message: '此项必填' }]}
+                >
+                  <InputNumber
+                    min={0}
+                    className="w-200"
+                    placeholder="请输入内容"
+                  />
+                </Form.Item>
+                <Form.Item
+                  labelCol={{ span: 7 }}
+                  label="整箱体积"
+                  name="h"
+                  rules={[{ required: true, message: '此项必填' }]}
+                >
+                  <InputNumber
+                    min={0}
+                    className="w-200"
+                    placeholder="请输入内容"
+                  />
+                </Form.Item>
               </div>
             </div>
           </div>
 
           <div>
-            <div className="divider mb-24">
-              <div className="text-#888 inline-block text-20 fw-600 mb-12">
+            <div className="divider mb-32">
+              <div className="text-#666 inline-block text-20 fw-600 mb-12">
+                物流运输
+              </div>
+            </div>
+
+            <div className="fi space-x-24">
+              <Form.Item
+                label="运输方式"
+                name="ysfs"
+                rules={[{ required: true, message: '此项必填' }]}
+              >
+                <Select
+                  placeholder="请选择"
+                  className="w-200!"
+                  options={[{ value: 'lucy', label: 'Lucy' }]}
+                />
+              </Form.Item>
+              <Form.Item
+                label="运输付款方式"
+                name="byclfs"
+                rules={[{ required: true, message: '此项必填' }]}
+              >
+                <Select
+                  placeholder="请选择"
+                  className="w-200!"
+                  options={[{ value: 'lucy', label: 'Lucy' }]}
+                />
+              </Form.Item>
+              <Form.Item
+                label="运费"
+                name="byclfs"
+                rules={[{ required: true, message: '此项必填' }]}
+              >
+                <InputNumber
+                  min={0}
+                  className="w-200"
+                  placeholder="请输入内容"
+                />
+              </Form.Item>
+            </div>
+          </div>
+
+          <div>
+            <div className="divider mb-32">
+              <div className="text-#666 inline-block text-20 fw-600 mb-12">
                 开票信息
               </div>
+            </div>
+            <div className="fi space-x-24">
+              <Form.Item label="增值税发票" name="byclfs">
+                <Switch />
+              </Form.Item>
+              <Form.Item label="普通发票" name="byclfs">
+                <Switch />
+              </Form.Item>
             </div>
           </div>
         </div>
       </Form>
 
       <footer
-        className="h-150 absolute bottom-0 left-0 w-full flex items-center justify-end px-45
+        className="h-130 absolute bottom-0 left-0 w-full flex items-center 
      "
       >
-        <span>合计： </span>
+        <div>
+          <span className="text-18 text-#888 mr-12 align-text-bottom">
+            合计
+          </span>
+          <span className="linear-text inline-block text-40 fw-600">12323</span>
 
-        <span className="linear-text inline-block text-40 fw-600"> 12323 </span>
+          <div className="mt-16">
+            <Button
+              shape="round"
+              size="large"
+              type="primary"
+              className="w-full"
+            >
+              提交
+            </Button>
+          </div>
+        </div>
       </footer>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPageWithLayout } from './_app'
-import { Form, Input, Button } from 'antd'
+import { Card } from 'antd'
 import MainForm from '@/components/MainForm'
 const Home: NextPageWithLayout = () => {
-  return <MainForm />
+  return (
+    <Card bordered={false}>
+      <MainForm />{' '}
+    </Card>
+  )
 }
 export default Home
